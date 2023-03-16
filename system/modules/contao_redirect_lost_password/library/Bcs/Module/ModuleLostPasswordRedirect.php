fc58aed15f9fbc88086e8d5ae528736406dfea7c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-16 13:01:23 -0400

    Update ModuleLostPasswordRedirect.php

--- system/modules/contao_redirect_lost_password/library/Bcs/Module/ModuleLostPasswordRedirect.php
+++ system/modules/contao_redirect_lost_password/library/Bcs/Module/ModuleLostPasswordRedirect.php
@@ -14,7 +14,7 @@ namespace Bcs\Module;
 use Contao;
 use Contao\Config;
 
-class ModuleLostPasswordRedirect extends \Contao\ModuleLogin
+class ModuleLostPasswordRedirect extends \Contao\ModuleLostPassword
 {
     public function generate()
     {
@@ -28,6 +28,35 @@ class ModuleLostPasswordRedirect extends \Contao\ModuleLogin
         parent::compile();
         
         // This is where our custom code will go
-        
     }
+    
+    protected function sendPasswordLink($objMember)
+	{
+		$optIn = System::getContainer()->get('contao.opt_in');
+		$optInToken = $optIn->create('pw', $objMember->email, array('tl_member'=>array($objMember->id)));
+
+		// Prepare the simple token data
+		$arrData = $objMember->row();
+		$arrData['activation'] = $optInToken->getIdentifier();
+		$arrData['domain'] = Idna::decode(Environment::get('host'));
+		//$arrData['link'] = Idna::decode(Environment::get('url')) . Environment::get('requestUri') . ((strpos(Environment::get('requestUri'), '?') !== false) ? '&' : '?') . 'token=' . $optInToken->getIdentifier();
+        $arrData['link'] = "bingbongnoise" . 'token=' . $optInToken->getIdentifier();
+
+		// Send the token
+		$optInToken->send(
+			sprintf($GLOBALS['TL_LANG']['MSC']['passwordSubject'], Idna::decode(Environment::get('host'))),
+			System::getContainer()->get('contao.string.simple_token_parser')->parse($this->reg_password, $arrData)
+		);
+
+		System::getContainer()->get('monolog.logger.contao.access')->info('A new password has been requested for user ID ' . $objMember->id . ' (' . Idna::decodeEmail($objMember->email) . ')');
+
+		// Check whether there is a jumpTo page
+		if (($objJumpTo = $this->objModel->getRelated('jumpTo')) instanceof PageModel)
+		{
+			$this->jumpToOrReload($objJumpTo->row());
+		}
+
+		$this->reload();
+	}
+    
 }
