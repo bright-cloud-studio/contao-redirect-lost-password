939df5093919c2bfd0059f50004c3c86d8636ea9
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-16 12:01:35 -0400

    Create config.php

---
+++ system/modules/contao_redirect_lost_password/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Redirect Lost Password
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-redirect-lost-password
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+$GLOBALS['FE_MOD']['user']['login'] = 'Bcs\Module\ModuleLostPasswordRedirect';
