e11e8f50495f5156afa28f132476d9b81c25cc9d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-16 12:00:43 -0400

    Create autoload.php

---
+++ system/modules/contao_redirect_lost_password/config/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Redirect Lost Password
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-redirect-lost-password
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Register the classes */
+ClassLoader::addClasses(array
+(
+    'Bcs\Module\ModuleLostPasswordRedirect'               => 'system/modules/contao_redirect_lost_password/library/Bcs/Module/ModuleLostPasswordRedirect.php'
+));
