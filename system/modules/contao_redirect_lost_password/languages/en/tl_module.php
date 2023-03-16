cf4cbf12d8403905dec39700dcb14cbb2959f849
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-16 12:04:08 -0400

    Create tl_module.php

---
+++ system/modules/contao_redirect_lost_password/languages/en/tl_module.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Redirect Lost Password
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-redirect-lost-password
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_module']['jumpToAlternative']          = array('Redirect (Lost Password) page', 'Please choose the alternative page to send users to when returning from lost password\'s email link');
