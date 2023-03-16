ea18acf577f1485c0a0ff4661d3e1d8a04d604d4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-16 12:02:16 -0400

    Create tl_module.php

---
+++ system/modules/contao_redirect_lost_password/dca/tl_module.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Bright Cloud Studio's Contao Redirect Lost Password
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-redirect-lost-password
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+ /* Extend the tl_module palettes */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['login'] = str_replace(',redirectBack;', ',jumpToFailed,redirectBack;', $GLOBALS['TL_DCA']['tl_module']['palettes']['login']);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['jumpToFailed'] = array
+(
+  'label'                   => &$GLOBALS['TL_LANG']['tl_module']['jumpToFailed'],
+  'inputType'               => 'pageTree',
+  'foreignKey'              => 'tl_page.title',
+  'eval'                    => array('fieldType'=>'radio'),
+  'sql'                     => "int(10) unsigned NOT NULL default 0",
+  'relation'                => array('type'=>'hasOne', 'load'=>'lazy')
+);
